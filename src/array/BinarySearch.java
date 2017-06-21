4545a8c7edb5e609969f542783cdafee02fb3850
chhatrachhorm <chhatra.kit@gmail.com>
2017-06-21 08:27:55 +0700

    Binary Search

--- src/array/BinarySearch.java
+++ src/array/BinarySearch.java
@@ -1,19 +1,21 @@
 package array;
 import java.util.Scanner;
-public class LinearSearch {
+public class BinarySearch {
 
-	public static int linearSearch(int a[], int ele, int size){
-		int pos = -1;
-		for(int i=0; i<size; i++){
-			if(a[i]==ele){
-				pos = i;
-				return pos;
-			}
+	public static int binarySearch(int a[], int ele, int size){
+		int pos = -1, beg=0, mid=0, last =size-1, i=0;
+		while(i<size){
+			mid = (beg+last)/2;
+			if(ele == a[mid]){
+				return mid;
+			}else if(ele>a[mid]){
+				beg = mid + 1;
+			}else last = mid -1;
 		}
 		return pos;
 	}
 	public static void main(String[] args) {
-		int a[], ele, pos, n;
+		int a[], ele, pos, n, temp;
 		/*
 		 * if []a then the other variables have to be an array type
 		 * */
@@ -27,7 +29,18 @@ public class LinearSearch {
 			System.out.println("Enter the Elements you want to search: ");
 			ele = scan.nextInt();
 		}
-		pos = linearSearch(a, ele, n);
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n-1; j++){
+				if(a[j]>a[j+1]){
+					temp = a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0; i<n; i++)System.out.println(a[i]);
+		pos = binarySearch(a, ele, n);
+		System.out.println(pos);
 		if(pos==-1)System.out.println("The element you want to find is not found");
 		else System.out.println("The element is at "+ ++pos +" position");
 	}
