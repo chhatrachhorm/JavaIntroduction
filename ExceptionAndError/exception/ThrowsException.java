06c27b45f8d9bd9461a8e310b8c96b62fec94b29
chhatrachhorm <chhatra.kit@gmail.com>
2017-06-27 10:07:23 +0700

    throws

---
+++ ExceptionAndError/exception/ThrowsException.java
@@ -0,0 +1,25 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+
+	public static void openFile(String path) throws FileNotFoundException{
+		/*
+		 * throws will throw back the exception to the function call
+		 * function call has to handle the exception with try catch and/or with finally
+		 * */
+		FileInputStream fis = new FileInputStream(path);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			openFile("D:\\html.html");
+		}catch(FileNotFoundException e){
+			System.out.println("Exception occured: "+e);
+		}
+	}
+
+
+}
