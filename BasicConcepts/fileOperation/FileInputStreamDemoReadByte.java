76b4d7ba554a54480b5d93c50ccc34359007f05c
chhatrachhorm <ch.chhatra@gmail.com>
2017-07-25 08:55:49 +0700

    FileOperation

---
+++ BasicConcepts/fileOperation/FileInputStreamDemoReadByte.java
@@ -0,0 +1,23 @@
+package fileOperation;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamDemoReadByte {
+    public static void main(String args[]) {
+        String str;
+        byte b[] = new byte[500];
+        try {
+            FileInputStream fis = new FileInputStream("BasicConcepts/fileOperation/FIleStorage/fileDemo.txt");
+            System.out.println("Other method with read(byte array)");
+            fis.read(b);
+            System.out.println(b);
+            str = new String(b);
+            System.out.println(str.trim());
+//            use trim() to get only the value not the unwanted square box
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
